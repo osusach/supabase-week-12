bd6126104a810772be64439e33c1289af407e0f1
Ignacio Díaz <ignacio.db@icodesx.com>
2024-09-18 00:08:28 -0300

    feat: add root layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
+
 import "@/app/globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const metadata = {
+export const metadata: Metadata = {
+  description:
+    "PathwayAid helps students find personalized scholarships and financial aid opportunities based on their academic interests and background. Easily browse a vast directory of scholarships or get matched with opportunities tailored to you. Start your journey towards a debt-free education today!",
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: {
+    default: "PathwayAid — Find Scholarships and Financial Aid for College",
+    template: "%s | PathwayAid",
+  },
 };
 
 export default function RootLayout({
